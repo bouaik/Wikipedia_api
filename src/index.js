e6d32a98e947bee53c9afaf68be18b5ac1cb221d
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-19 00:45:16 +0100

    Show result in dom

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ const output = document.querySelector('.output')
 const search = document.getElementById('search')
 const feedback = document.querySelector('.feedback')
 
-const base = `https://en.wikipedia.org/w/api.php`
-const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=`
+const url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch='
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -22,6 +21,7 @@ searchForm.addEventListener('submit', (e) => {
     } else {
         search.value = ""
         // ajax
+        ajaxWiki(value)
     }
 })
 
@@ -33,4 +33,36 @@ const showFeedback = (text) => {
     setTimeout(() => {
         feedback.classList.remove('showItem')
     }, 3000)
+}
+
+const ajaxWiki = (search) => {
+    output.innerHTML = ""
+    loading.classList.add('showItem')
+
+    const wikiURL = `${url}${search}`
+
+    fetch(wikiURL)
+        .then(data => data.json())
+        .then(data => displayData(data))
+        .catch(error => console.log(error))
+}
+
+const displayData = (data) => {
+    loading.classList.remove('showItem')
+    const { search:results } = data.query
+
+    let info = ''
+
+    results.forEach(result => {
+        const {title, snippet, pageid:link} = result 
+        info += `<div class="col-10 mx-auto col-md-6 col-lg-4 my-3">
+                    <div class="card card-body">
+                        <h1 class="card-title blueText">${title}</h1>
+                        <p>${snippet}</p>
+                        <a href="http://en.wikipedia.org/?curid=${link}" target="_blank" class="my-2 text-capitalize">read
+                        more...</a>
+                    </div>
+                </div>`
+    });
+    output.innerHTML = info
 }
